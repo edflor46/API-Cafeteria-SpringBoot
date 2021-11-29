7b3e45f94f3b81674bc821b27b7742524d012200
Eduardo Flores <luis.flores.ramirez@accenture.com>
2021-11-29 10:47:03 -0600

    ApiRest Started

---
+++ src/main/java/com/edflor/cafeteria/CafeteriaApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.edflor.cafeteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeteriaApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeteriaApiRestApplication.class, args);
+	}
+
+}
