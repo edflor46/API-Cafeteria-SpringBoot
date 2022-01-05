9857c46235e8dfe4239a211b8ed4407c174294e9
Eduardo Flores <luis.flores.ramirez@accenture.com>
2022-01-05 10:17:54 -0600

    API Cafeteria

---
+++ README.md
@@ -0,0 +1,5 @@
+# API Cafeteria
+
+- Springboot
+- Hibernate
+- JPA
--- src/main/java/com/edflor/cafeteria/controllers/CafeController.java
+++ src/main/java/com/edflor/cafeteria/controllers/CafeController.java
@@ -3,11 +3,17 @@ package com.edflor.cafeteria.controllers;
 import com.edflor.cafeteria.entity.Cafe;
 import com.edflor.cafeteria.services.ICafeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/")
@@ -19,6 +25,30 @@ public class CafeController {
     @GetMapping("/cafes")
     public List<Cafe> getCafeList() {
         return cafeService.findAll();
+    }
+
+    @GetMapping("/cafe/{id}")
+    public ResponseEntity<?> getCafeById(@PathVariable Long id) {
+
+        Cafe cafe = null;
+        Map<String, Object> response = new HashMap<String, Object>();
+
+        try {
+            cafe = cafeService.findById(id);
+        } catch (DataAccessException e){
+            response.put("mensaje", "Error en la peticion");
+            response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+        }
+
+        if (cafe == null){
+            response.put("mensaje", "El producto con el id: ".concat(id.toString().concat(" no es valido")));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<Cafe>(cafe, HttpStatus.OK);
+
+
     }
 
 }
